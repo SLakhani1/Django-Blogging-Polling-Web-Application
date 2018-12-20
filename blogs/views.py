82b6c25e9e1ca2e5ba9e1fc5f9ca22bc28b1a045
SLakhani1 <sagar.lakhani63@gmail.com>
2018-12-20 23:51:05 +0530

    Added comment feature

--- blogs/views.py
+++ blogs/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
 from django.utils import timezone
-from .forms import PostForm
+from .forms import PostForm,CommentForm
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -37,3 +37,16 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blogs/post_edit.html', {'form': form})
+
+def add_comment_to_post(request,pk):
+    post = get_object_or_404(Post,pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('blogs:post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blogs/add_comment_to_post.html', {'form': form})
